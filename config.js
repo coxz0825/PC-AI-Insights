f74284f4fef564c8239e1cdb2e456c0721c9e77a
coxz0825 <229745703@qq.com>
2026-08-05 17:14:20 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.LOCAL_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyALayTmut_Qt5HHfeDwK-IxUEzLxFujIK8",
+    authDomain: "pc-ai-insights.firebaseapp.com",
+    databaseURL: "https://pc-ai-insights-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "pc-ai-insights",
+    storageBucket: "pc-ai-insights.firebasestorage.app",
+    messagingSenderId: "676319801033",
+    appId: "1:676319801033:web:fe725fd11529ebef886718",
+    measurementId: "G-4YTL42N6C3"
+};
